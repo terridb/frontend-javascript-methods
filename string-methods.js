d115df6fedceebfcae22327e110c563876a4c81c
terridb <terrideboer99@gmail.com>
2025-01-04 12:26:21 +0100

    Opdracht strings afgerond

--- string-methods.js
+++ string-methods.js
@@ -12,7 +12,7 @@ console.log(word.length);
 
 const fruit = "Banaan";
 const vegetable = "Aardappel";
-
+console.log(fruit.length > vegetable.length);
 
 
 /* Opdracht 2 */
@@ -20,15 +20,15 @@ const vegetable = "Aardappel";
 //  Tip: bedenk 1 manier die voor beide woorden zou werken.
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
-
-
+console.log(fruit.charAt(fruit.length - 1));
+console.log(vegetable.charAt(vegetable.length - 1));
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
+console.log(userInput.trim());
 
 
 /* Opdracht 4 */
@@ -36,13 +36,13 @@ const userInput = "  De kat krabt de krullen van de trap ";
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
+console.log(story.includes("Fantastisch"));
 
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
-
+console.log(story.substring(0, 52));
 
 
 /* Opdracht 6 */
@@ -53,8 +53,7 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
+const euroPrice = price.replaceAll("$", "€").replaceAll(".", ",");
 
-
-
-
+console.log(euroPrice);
 
